adc73b4d1813c7fc8b1c5b38802df665f124ca59
Le Coat Frederic <danaleight@dc1.ad.fredlecoat.com>
2025-12-18 17:29:30 +0100

    initialisation du projet

---
+++ backend/src/main/java/com/fredlecoat/theme_park_industries_scrap/ThemeParkIndustriesScrapApplication.java
@@ -0,0 +1,13 @@
+package com.fredlecoat.theme_park_industries_scrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeParkIndustriesScrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemeParkIndustriesScrapApplication.class, args);
+	}
+
+}
